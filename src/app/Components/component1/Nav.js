4f7093cac8f0b78ff9b0c500075701557f77dae3
Manoj-2301 <manojthapa.uiux@gmail.com>
2025-01-29 10:54:15 +0530

    did change on 29 jan

--- src/app/Components/component1/Nav.js
+++ src/app/Components/component1/Nav.js
@@ -1,35 +1,39 @@
+"use client";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { IoBagOutline, IoChevronDownOutline } from "react-icons/io5";
 import "./style.scss";
 import Button from "../button/Button";
 
 const Nav = () => {
+  const [Scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const nav = [
-    {
-      name: "Home",
-      path: "/Home",
-    },
-    ,
-    {
-      name: "About",
-      path: "/About",
-    },
-    {
-      name: "Feature",
-      path: "/Feature",
-    },
-    {
-      name: "Contact ",
-      icon: <IoChevronDownOutline />,
-      path: "/Contact",
-    },
-    {
-      name: "Pricing",
-      path: "/Pricing",
-    },
+    { name: "Home", path: "/Home" },
+    { name: "About", path: "/About" },
+    { name: "Feature", path: "/Feature" },
+    { name: "Contact ", icon: <IoChevronDownOutline />, path: "/Contact" },
+    { name: "Pricing", path: "/Pricing" },
   ];
+
   return (
-    <div className="navBrand">
+    <div className={`navBrand ${Scrolled ? "scrolled" : ""}`}>
       <div className="nav">
         <div className="nav__item">
           <div className="logo">
@@ -38,8 +42,8 @@ const Nav = () => {
             </Link>
           </div>
           <div className="nav_item">
-            {nav.map((nav) => (
-              <div>
+            {nav.map((nav, index) => (
+              <div key={index}>
                 <Link href={nav.path} className="navItem">
                   <p>
                     {nav.name}
